a39c1832d83d9747c842e2516be1ec6a9ff0900f
no-way02 <62077153+no-way02@users.noreply.github.com>
2023-06-29 15:57:22 +0530

    AngularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: 
+  `<div>
+      <h1> {{pageTitle}}</h1>
+      <pm-products></pm-products>
+
+  </div>` 
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apm-new';
+  pageTitle: String = 'Home Inventory';
 }
